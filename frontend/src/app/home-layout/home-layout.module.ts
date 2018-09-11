8108a46738bd7663386c95a6ada6a54dcd873ffa
hda-narola <hda@narola.email>
2018-09-11 12:58:19 +0530

    initial commit

---
+++ frontend/src/app/home-layout/home-layout.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        
+        children: [
+          
+        ]
+      }
+    ])
+  ],
+  declarations: [ ],
+  providers : []
+})
+export class HomeLayoutModule { }
---
+++ frontend/src/app/home-layout/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: []
+})
+export class LayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
