b4305d83b074a6a2ef7cb0a15cf4b7dc22a49133
sisnarola <sis@narola.email>
2018-09-18 12:26:22 +0530

    changes

--- frontend/src/app/main-layout/cow-setup/livestock-agents/livestock-agents.component.ts
+++ frontend/src/app/main-layout/cow-setup/livestock-agents/livestock-agents.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-livestock-agents',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./livestock-agents.component.css']
 })
 export class LivestockAgentsComponent implements OnInit {
-
-  constructor() { }
+  inviteAgentModalRef : BsModalRef;
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
   }
 
+  // Open Getting start modal
+  openAddAgentModal(template) {
+    this.inviteAgentModalRef = this.modalService.show(template, {class : 'get-started-popup', backdrop : true});
+  }
 }
